fe975c8b06f85325af66a19c85a4f37ededa6f6c
Rub4l1to <adrian.perez.garcia2018@gmail.com>
2019-05-06 12:55:06 +0200

    V.1.0.0 (Global)

---
+++ js/motor.js
@@ -0,0 +1,37 @@
+var perfiles = [{
+        "foto": "img/4.jpg",
+        "edificio": "img/edf1.jpg",
+        "nombre": "Baño",
+    },
+    {
+        "foto": "img/5.jpg",
+        "edificio": "img/edf2.jpg",
+        "nombre": "Cocina",
+    },
+    {
+        "foto": "img/6.jpg",
+        "edificio": "img/edf3.png",
+        "nombre": "Salon",
+    }
+];
+var x = 0;
+var count = NumeroAleatorio(1, 4);
+var foto = document.getElementsByClassName("cajita");
+var fotoin = document.getElementsByClassName("cajita2");
+
+document.write("<section id='galeria'>");
+document.write("<div id='contenedorI'>");
+for (x = 0; x < perfiles.length; x++) {
+    document.write("<div id='jk1' class='cajita'>");
+    foto[x].style.backgroundImage = "url('" + perfiles[x].foto + "')";
+    document.write("<div id='jk2' class='cajita2'>");
+    fotoin[x].style.backgroundImage = "url('" + perfiles[x].edificio + "')";
+    document.write("</div>");
+    document.write("</div>");
+}
+document.write("</div>");
+document.write("</section>");
+
+function NumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
