c7a1422056f00c1b7dc26d9b4c3c954577aa2cda
Mijayo <DIEGO@Diegos-MacBook-Pro.local>
2019-05-07 18:52:56 +0200

    v.Beta.3

--- js/motor.js
+++ js/motor.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 10) {
+            $(".black").css("boxShadow", "0 px - 5 px 13 px - 1 px rgba(214, 214, 214, 1)");
+        } else {
+            $(".black").css("background", "white");
+        }
+    });
+});
+
+
 var perfiles = [{
         "foto": "img/4.jpg",
         "edificio": "img/edf1.jpg",
